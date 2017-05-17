e98b454343c20c0947fd0125ada1102046aee061
ranemartin8 <ranemartin8@gmail.com>
2017-05-16 20:31:44 -0400

    updated

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
 const commando = require('discord.js-commando');
-const bot = new commando.Client();
-
-bot.on('ready', () => {
-  console.log('I am ready!');
+const bot = new commando.Client({
+    commandPrefix: '!',
+    owner: '308388415064506369',
+    disableEveryone: true,
+    unknownCommandResponse: false
 });
 
+//.on('ready', () => {
+ // console.log('I am ready!');
+//});
+
+//const guildSettings = require('./dataProviders/postgreSQL/models/GuildSettings');
+
+bot
+.on('error', console.error)
+	.on('warn', console.warn)
+	.on('debug', console.log)
+	.on('ready', () => {
+		console.log('Client ready');
+	})
+	.on('disconnect', () => { console.warn('Disconnected!'); })
+	.on('reconnect', () => { console.warn('Reconnecting...'); })
+	.on('commandError', (cmd, err) => {
+		if(err instanceof commando.FriendlyError) return;
+		console.error('Error in command ${cmd.groupID}:${cmd.memberName}', err);
+	});
+
 bot.registry.registerGroups([
-	['random', 'Random'],
-	['getpi','ChampPI'],
-	['group2','saysStuff']	
+	['champpi','Champ Information'],
+	['tools','Alliance Tools'],
+    ['content','MCOC Content']
+   // ['config','Configuations']
+
 	]);
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
 
-bot.on('message',(message) =>{
 
-	if(message.content == 'ping'){
-		message.channel.sendMessage('pong');
-	}
+bot.on('message',(message) =>{
+  if (message.author.bot) return;
+    
+//	if(message.content == 'ping'){
+//		message.channel.sendMessage('pong');
+//	}
+    
+   // if (message.content.replace(/ /g, '') == '!getthis')
 
 });
 
